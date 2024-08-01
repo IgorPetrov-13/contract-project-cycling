5ee315ed37face5f847a574c2c214a18f48f99ca
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-08-01 14:27:18 +0300

    feat/map was added

--- client/src/pages/RoadsPage.jsx
+++ client/src/pages/RoadsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import apiAxiosInstance from "../service/apiAxiosInstace";
 import MapComponent from "../components/MapComponent";
+import { Link } from "react-router-dom";
 
 function RoadsPage({ roads, setRoads }) {
   const getRoads = async () => {
@@ -18,8 +19,7 @@ function RoadsPage({ roads, setRoads }) {
       {roads.map((road) => {
         return (
           <section key={road.id}>
-          
-            <div>{road.title}</div>
+            <Link to={`/roads/${road.id}`}>{road.title}</Link>
             <br />
             <iframe
               src="https://yandex.ru/map-widget/v1/?um=constructor%3Af2d0a3fc3a4d61df6fe47a98d6f3e3318349c4346e34c663962f5c5a4d3b5bdf&amp;source=constructor"
