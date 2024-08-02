09fd670f29321f33924b49563bb56adfc9e00a88
NataSokol <nata28051995@icloud.com>
2024-08-02 15:07:44 +0300

    change

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,11 +2,14 @@ import Slider from "../components/Slider";
 
 function HomePage({ user }) {
   return (
-
-    <div style={{ width: "500px" , marginBottom: "50px", marginTop: "20px" }}>
-      <h2 style={{marginBottom: "50px"}}> Привет, {user ? user.name : "красавчик"}</h2>
-      <h4>
+    <div style={{ width: "500px", marginBottom: "50px", marginTop: "20px" }}>
+      <h2 style={{ marginBottom: "50px" }}>
         {" "}
+        Привет, {user ? user.name : "красавчик"}!{" "}
+      </h2>
+      <h4>
+      {user ? '': "Давай"}
+         {" "}
         {user ? (
           ""
         ) : (
@@ -15,28 +18,19 @@ function HomePage({ user }) {
             зарегистрируемся
           </a>
         )}{" "}
-      </h4>
-      <h4> {user ? "" : "или"} </h4>
-      <h4>
-        {" "}
+         {user ? '': "или "}
+        
         {user ? (
           ""
         ) : (
           <a href="http://localhost:5173/auth/authorization">
             {""}
-            войдем?
+            войдем
           </a>
-        )}{" "}
+        )}
+         {user ? '': "?"}
       </h4>
       <Slider />
-// 
-//     <div style={{ width: "500px" , marginBottom: "50px", marginTop: "20px" }}>
-//       <h2  style={{marginBottom: "50px"}}> Привет, {user ? user.name : "красавчик"} </h2>
-//       <h3  style={{marginBottom: "50px"}}> {user ? "" : "давай зарегистрируемся или войдем?"} </h3>
-//       <section   style={{marginBottom: "50px"}} className="d-flex flex-column justify-content-center align-items-center">
-//         <Slider />
-//       </section>
-// >>>>>>> dev
     </div>
   );
 }
